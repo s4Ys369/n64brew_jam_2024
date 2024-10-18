a604264df51159348009c23336ccf8d625d84b4a
Giovanni Bajo <rasky@develer.com>
2024-10-18 14:09:52 +0200

    polyquiz: improvements to the rendering, and some initial game mechanic

--- code/polyquiz/polyquiz.c
+++ code/polyquiz/polyquiz.c
@@ -25,7 +25,7 @@ typedef struct {
 
 typedef struct {
     int v1, v2, v3;
-    Color color;
+    int color_idx;
 } Face;
 
 Color palette[] = {
@@ -54,16 +54,34 @@ sprite_t *bkg = NULL;
 rdpq_font_t *font = NULL;
 #define FONT_TEXT 1
 
-float angleX = 0.0f;
-float angleY = 0.0f;
-float angleZ = 0.0f;
-float rotationSpeed = 20.0f;
+float angle = 0.0f;
+float rotationSpeed = 50.0f;
+float axisX = 0.0f, axisY = 1.0f, axisZ = 0.0f; 
+
+void generateRandomAxis() {
+    axisX = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+    axisY = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+    axisZ = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+
+    float length = sqrtf(axisX * axisX + axisY * axisY + axisZ * axisZ);
+    if (length > 0.0f) {
+        axisX /= length;
+        axisY /= length;
+        axisZ /= length;
+    }
+}
 
 Vertex random_vertex(float range_min, float range_max) {
     Vertex v;
-    v.x = range_min + (float)rand() / RAND_MAX * (range_max - range_min);
-    v.y = range_min + (float)rand() / RAND_MAX * (range_max - range_min);
-    v.z = range_min + (float)rand() / RAND_MAX * (range_max - range_min);
+
+    float radius = (range_max - range_min)/2;
+    float theta = ((float)rand() / RAND_MAX) * 2.0f * 3.1415628f;  // Angolo azimutale [0, 2*PI]
+    float phi = acosf(1.0f - 2.0f * ((float)rand() / RAND_MAX));  // Angolo polare [0, PI]
+
+    v.x = radius * sinf(phi) * cosf(theta);
+    v.y = radius * sinf(phi) * sinf(theta);
+    v.z = radius * cosf(phi);
+
     return v;
 }
 
@@ -125,7 +143,6 @@ void compute_convex_hull() {
                         faces[num_faces].v1 = p;
                         faces[num_faces].v2 = i;
                         faces[num_faces].v3 = j;
-                        faces[num_faces].color = random_color_from_palette();
                         num_faces++;
                     }
                 }
@@ -136,6 +153,37 @@ void compute_convex_hull() {
             p = 0;
         }
     } while (p != start && num_faces < MAX_FACES);
+}
+
+void color_polyhedron(void) {
+    int are_faces_adjacent(Face *f1, Face *f2) {
+        int shared_vertices = 0;
+        if (f1->v1 == f2->v1 || f1->v1 == f2->v2 || f1->v1 == f2->v3) shared_vertices++;
+        if (f1->v2 == f2->v1 || f1->v2 == f2->v2 || f1->v2 == f2->v3) shared_vertices++;
+        if (f1->v3 == f2->v1 || f1->v3 == f2->v2 || f1->v3 == f2->v3) shared_vertices++;
+
+        return (shared_vertices == 2);
+    }    
+    
+    int is_valid_color(Face *faces, int face_index, int color_index, int num_faces) {
+        for (int i = 0; i < num_faces; i++) {
+            if (i == face_index) continue;
+            if (are_faces_adjacent(&faces[face_index], &faces[i]) && faces[i].color_idx == color_index)
+                return false;
+        }
+        return true;
+    }
+
+
+    for (int i = 0; i < num_faces; i++) {
+        int idx = rand() % PALETTE_SIZE;
+        for (int c = 0; c < PALETTE_SIZE; c++) {
+            if (is_valid_color(faces, i, (idx+c)%PALETTE_SIZE, num_faces)) {
+                faces[i].color_idx = (idx+c) % PALETTE_SIZE;
+                break;
+            }
+        }
+    }
 }
 
 void draw_polyhedron(void)
@@ -152,7 +200,8 @@ void draw_polyhedron(void)
         normal.z = (v2.x - v1.x) * (v3.y - v1.y) - (v2.y - v1.y) * (v3.x - v1.x);
 
         glNormal3f(normal.x, normal.y, normal.z);
-        glColor4f(faces[i].color.r, faces[i].color.g, faces[i].color.b, 0.8f);
+        Color color = palette[faces[i].color_idx];
+        glColor4f(color.r, color.g, color.b, 0.8f);
 
         glVertex3f(v1.x, v1.y, v1.z);
         glVertex3f(v2.x, v2.y, v2.z);
@@ -167,6 +216,7 @@ void generate_random_polyhedron(int num_vertices_input, float range_min, float r
         vertices[i] = random_vertex(range_min, range_max);
     }
     compute_convex_hull();
+    color_polyhedron();
 
     if (poly) rspq_block_free(poly);
     rspq_block_begin();
@@ -176,7 +226,7 @@ void generate_random_polyhedron(int num_vertices_input, float range_min, float r
 
 void minigame_init()
 {
-    display_init(RESOLUTION_640x480, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE);
+    display_init(RESOLUTION_640x480, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS);
     gl_init();
 
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);  // Colore di sfondo
@@ -198,12 +248,12 @@ void minigame_init()
     glEnable(GL_COLOR_MATERIAL);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 
-    float aspect_ratio = (float)display_get_width() / (float)display_get_height();
+    int w = display_get_width(), h = display_get_height();
     float near_plane = 1.0f;
     float far_plane = 50.0f;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glFrustum(-near_plane*aspect_ratio, near_plane*aspect_ratio, -near_plane, near_plane, near_plane, far_plane);
+    gluPerspective(45.0, (GLfloat)w / (GLfloat)h, near_plane, far_plane);
 
     int num_vertices = rand() % 10 + 5;
     generate_random_polyhedron(num_vertices, -1.0f, 1.0f);
@@ -211,6 +261,13 @@ void minigame_init()
     bkg = sprite_load("rom:/polyquiz/plaster2.ci4.sprite");
     font = rdpq_font_load("rom:/polyquiz/abaddon.font64");
     rdpq_text_register_font(FONT_TEXT, font);
+    rdpq_font_style(font, 0, &(rdpq_fontstyle_t){
+        .color = RGBA32(0xFF, 0xFF, 0xFF, 0xFF), .outline_color = RGBA32(0x0, 0x0, 0x0, 0xFF),
+    });
+    rdpq_font_style(font, 1, &(rdpq_fontstyle_t){
+        .color = RGBA32(palette[2].r*255, palette[2].g*255, palette[2].b*255, 0xFF),
+        .outline_color = RGBA32(0x0, 0x0, 0x0, 0xFF),
+    });
 }
 
 void minigame_cleanup()
@@ -225,18 +282,40 @@ void minigame_cleanup()
 
 void minigame_fixedloop(float dt)
 {
-    angleX += rotationSpeed * dt;
-    angleY += rotationSpeed * dt;
-    angleZ += rotationSpeed * dt;
-    if (angleX > 360.0f) angleX -= 360.0f;
-    if (angleY > 360.0f) angleY -= 360.0f;
-    if (angleZ > 360.0f) angleZ -= 360.0f;
+    angle += rotationSpeed * dt;
+    if (angle > 360.0f) {
+        angle -= 360.0f;
+
+        // Cambia l'asse di rotazione ogni volta che l'angolo raggiunge 360 gradi
+        generateRandomAxis();
+    }
 }
+
+struct {
+    int guess;
+    bool confirmed;
+    float confirm_time;
+} player[4];
 
 void minigame_loop(float dt)
 {
     joypad_buttons_t btn = joypad_get_buttons_pressed(JOYPAD_PORT_1);
-    if (btn.start) minigame_end();
+    if (btn.start) {
+        debugf("Minigame ended by player (faces=%d)\n", num_faces);
+        minigame_end();
+    }
+
+    for (int i=0; i<4; i++) {
+        if (player[i].confirmed) break;
+        joypad_buttons_t btn = joypad_get_buttons_pressed(JOYPAD_PORT_1+i);
+        if (btn.d_up) player[i].guess++;
+        if (btn.d_down) player[i].guess--;
+        if (btn.d_left) player[i].guess -= 10;
+        if (btn.d_right) player[i].guess += 10;
+        if (player[i].guess < 0) player[i].guess = 0;
+        if (player[i].guess > 30) player[i].guess = 30;
+        if (btn.a) player[i].confirmed = true;
+    }
 
     surface_t *disp = display_get();
     rdpq_attach(disp, NULL);
@@ -249,19 +328,13 @@ void minigame_loop(float dt)
 
     gl_context_begin();
 
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // glClear(GL_COLOR_BUFFER_BIT);
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0.0, 0.0, 3.0,  // Camera pos
+    gluLookAt(0.0, 0.0, 4.0,  // Camera pos
               0.0, 0.0, 0.0,  // Look at
               0.0, 1.0, 0.0); // Up vector
 
-    glScalef(1.2f, 1.2f, 1.2f);
-    glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-    glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-    glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
+    glRotatef(angle, axisX, axisY, axisZ);
 
     rspq_block_run(poly);
 
@@ -271,6 +344,13 @@ void minigame_loop(float dt)
     rdpq_text_printf(&(rdpq_textparms_t){
         .width = display_get_width(), .align = ALIGN_CENTER,
     }, FONT_TEXT, 0, 50, "Guess the number of faces!");
+
+    for (int i=0; i<4; i++) {
+        rdpq_textparms_t parms = {
+            .style_id = player[i].confirmed ? 1 : 0,
+        };
+        rdpq_text_printf(&parms, FONT_TEXT, 100+i*100, 460, "%d", player[i].guess);
+    }
 
     rdpq_detach_show();
 }
