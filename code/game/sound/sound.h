0b1314da62822dfb115532ebafcdc628061b03d6
s4Ys369 <twitchversusmarioneat@gmail.com>
2024-10-22 12:02:37 -0400

    allman braces

--- code/game/sound/sound.h
+++ code/game/sound/sound.h
@@ -19,7 +19,8 @@ const char* wavFileNames[1] = {
 
 void sound_load(void);
 
-void sound_load(void) {
+void sound_load(void)
+{
 	wav64_open(&sfx_boing, wavFileNames[0]);
     xm64player_open(&xmPlayer, xmFileNames[0]);
     xm64player_play(&xmPlayer, MUSIC_CHANNEL);
@@ -27,7 +28,8 @@ void sound_load(void) {
 
 void sound_xm_switch(int songID);
 
-void sound_xm_switch(int songID){
+void sound_xm_switch(int songID)
+{
     xm64player_stop(&xmPlayer);
 	xm64player_close(&xmPlayer);
 	xm64player_open(&xmPlayer, xmFileNames[songID]);
@@ -38,14 +40,16 @@ void sound_xm_switch(int songID){
 
 void sound_xm_stop(void);
 
-void sound_xm_stop(void){
+void sound_xm_stop(void)
+{
 	xm64player_stop(&xmPlayer);
 	xm64player_close(&xmPlayer);
 }
 
 void sound_xm_update(bool isLooping, float volume);
 
-void sound_xm_update(bool isLooping, float volume){
+void sound_xm_update(bool isLooping, float volume)
+{
 	xm64player_set_loop(&xmPlayer, isLooping);
 	xm64player_set_vol(&xmPlayer, volume);
 }
@@ -53,18 +57,20 @@ void sound_xm_update(bool isLooping, float volume){
 
 void sound_wav_bounce(void);
 
-void sound_wav_bounce(void){
+void sound_wav_bounce(void)
+{
 	wav64_play(&sfx_boing, SFX_CHANNEL);
 }
 
 void sound_update_buffer(void);
 
-void sound_update_buffer(void) {
-    //if (audio_can_write()) {
-	//	short *buf = audio_write_begin();
-	//	mixer_poll(buf, audio_get_buffer_length());
-	//	audio_write_end();
-	//}
+void sound_update_buffer(void)
+{
+    /*if (audio_can_write()){
+		short *buf = audio_write_begin();
+		mixer_poll(buf, audio_get_buffer_length());
+		audio_write_end();
+	}*/
 	mixer_try_play();
 }
 
