ac749e75cf5b2d9e76e37cb42c03973c8fdcbbd3
s4Ys369 <twitchversusmarioneat@gmail.com>
2024-11-17 22:35:27 -0500

    disable AA

--- code/game/screen/screen.h
+++ code/game/screen/screen.h
@@ -16,7 +16,7 @@ void screen_clearDisplay(Screen* screen);
 
 void screen_initDisplay(Screen* screen)
 {
-	display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS);
+	display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE);
 	screen->depthBuffer = surface_alloc(FMT_RGBA16, display_get_width(), display_get_height());
 }
 
